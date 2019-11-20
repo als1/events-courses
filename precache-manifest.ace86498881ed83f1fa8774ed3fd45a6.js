31a4a4648ec0d02d50a6321f7d6015e10ea81877
al <deehun@gmail.com>
2019-11-20 01:52:25 -0800

    Updates

---
+++ precache-manifest.ace86498881ed83f1fa8774ed3fd45a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d17c10f015d5269d11c036c8c6c68360",
+    "url": "/events-courses/index.html"
+  },
+  {
+    "revision": "7e552f647d1f577b55c4",
+    "url": "/events-courses/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "782d2bfb0262554b7721",
+    "url": "/events-courses/static/css/main.f0cd3304.chunk.css"
+  },
+  {
+    "revision": "7e552f647d1f577b55c4",
+    "url": "/events-courses/static/js/2.18bedff0.chunk.js"
+  },
+  {
+    "revision": "782d2bfb0262554b7721",
+    "url": "/events-courses/static/js/main.2bceaeab.chunk.js"
+  },
+  {
+    "revision": "101c3cfb8ac434ef2b4f",
+    "url": "/events-courses/static/js/runtime-main.1a674eab.js"
+  }
+]);
