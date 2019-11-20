17286a0520f119bea3f905a081a82eb83c9480ba
al <deehun@gmail.com>
2019-11-20 12:03:34 -0800

    Shortened counter label

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Cart = (props) => {
     return (<nav className="navbar navbar-dark bg-dark">
     <div className="navbar-brand cart-container" href="#">
-        <span>Total Registered Students: </span>
+        <span>Registered: </span>
         <span className="badge badge-pill badge-info">{props.totalItems}</span>
     </div>
 </nav>);
